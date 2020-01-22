6e0b35b9d9b6f15313e8c633761240bf1da1f0c9
oliver tuuling <oliver.tuuling@tptlive.ee>
2020-01-22 18:24:23 +0200

    Polish code

--- GameActivity.cs
+++ GameActivity.cs
@@ -14,20 +14,72 @@ using Android.Widget;
 namespace Connect_four
 {
     [Activity(Label = "GameActivity")]
-    public class GameActivity : Activity
+    public class GameActivity : Activity 
     {
-        //private TextView[] aColumn = new TextView[6];
+        private TextView[] aColumn = new TextView[6];
+        private TextView[] bColumn = new TextView[6];
+        private TextView[] cColumn = new TextView[6];
+        private TextView[] dColumn = new TextView[6];
+        private TextView[] eColumn = new TextView[6];
+        private TextView[] fColumn = new TextView[6];
+        private TextView[] gColumn = new TextView[6];
+        private Button[] gameButton = new Button[7];
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.grid_layout);
-            //for (int i = 0; i < 6; i++)
-            //{
-            //    var textViewID = "A" + i;
-            //    aColumn[i] = FindViewById<TextView>();
+            for (int i = 1; i <= 6; i++)
+            {
+                var textViewId = $"A{i}";
+                var view = FindViewById<TextView>(Resources.GetIdentifier(textViewId, "id", PackageName));
+                aColumn[i] = view;
+            }
 
-            //}
+            for (int i = 1; i <= 6; i++)
+            {
+                var textViewId = $"B{i}";
+                var view = FindViewById<TextView>(Resources.GetIdentifier(textViewId, "id", PackageName));
+            }
+
+            for (int i = 1; i <= 6; i++)
+            {
+                var textViewId = $"C{i}";
+                var view = FindViewById<TextView>(Resources.GetIdentifier(textViewId, "id", PackageName));
+            }
+
+            for (int i = 1; i <= 6; i++)
+            {
+                var textViewId = $"D{i}";
+                var view = FindViewById<TextView>(Resources.GetIdentifier(textViewId, "id", PackageName));
+            }
+
+            for (int i = 1; i <= 6; i++)
+            {
+                var textViewId = $"E{i}";
+                var view = FindViewById<TextView>(Resources.GetIdentifier(textViewId, "id", PackageName));
+            }
+
+            for (int i = 1; i <= 6; i++)
+            {
+                var textViewId = $"F{i}";
+                var view = FindViewById<TextView>(Resources.GetIdentifier(textViewId, "id", PackageName));
+            }
+
+            for (int i = 1; i <= 6; i++)
+            {
+                var textViewId = $"G{i}";
+                var view = FindViewById<TextView>(Resources.GetIdentifier(textViewId, "id", PackageName));
+            }
+
+            for (int i = 1; i <= 7; i++)
+            {
+                var buttonId = $"gameButton{i}";
+                var view = FindViewById<Button>(Resources.GetIdentifier(buttonId, "id", PackageName));
+            }
             // Create your application here 
+
+            
+            
 
             // gamebuttons
             var gameButton1 = FindViewById<Button>(Resource.Id.gameButton1);
@@ -38,70 +90,23 @@ namespace Connect_four
             var gameButton6 = FindViewById<Button>(Resource.Id.gameButton6);
             var gameButton7 = FindViewById<Button>(Resource.Id.gameButton7);
 
-            // aColumn
-            var A1 = FindViewById<TextView>(Resource.Id.A1);
-            var A2 = FindViewById<TextView>(Resource.Id.A2);
-            var A3 = FindViewById<TextView>(Resource.Id.A3);
-            var A4 = FindViewById<TextView>(Resource.Id.A4);
-            var A5 = FindViewById<TextView>(Resource.Id.A5);
-            var A6 = FindViewById<TextView>(Resource.Id.A6);
-
-            // bColumn
-            var B1 = FindViewById<TextView>(Resource.Id.B1);
-            var B2 = FindViewById<TextView>(Resource.Id.B2);
-            var B3 = FindViewById<TextView>(Resource.Id.B3);
-            var B4 = FindViewById<TextView>(Resource.Id.B4);
-            var B5 = FindViewById<TextView>(Resource.Id.B5);
-            var B6 = FindViewById<TextView>(Resource.Id.B6);
-
-            // cColumn
-            var C1 = FindViewById<TextView>(Resource.Id.C1);
-            var C2 = FindViewById<TextView>(Resource.Id.C2);
-            var C3 = FindViewById<TextView>(Resource.Id.C3);
-            var C4 = FindViewById<TextView>(Resource.Id.C4);
-            var C5 = FindViewById<TextView>(Resource.Id.C5);
-            var C6 = FindViewById<TextView>(Resource.Id.C6);
-
-            // dColumn
-            var D1 = FindViewById<TextView>(Resource.Id.D1);
-            var D2 = FindViewById<TextView>(Resource.Id.D2);
-            var D3 = FindViewById<TextView>(Resource.Id.D3);
-            var D4 = FindViewById<TextView>(Resource.Id.D4);
-            var D5 = FindViewById<TextView>(Resource.Id.D5);
-            var D6 = FindViewById<TextView>(Resource.Id.D6);
-
-            // eColumn
-            var E1 = FindViewById<TextView>(Resource.Id.E1);
-            var E2 = FindViewById<TextView>(Resource.Id.E2);
-            var E3 = FindViewById<TextView>(Resource.Id.E3);
-            var E4 = FindViewById<TextView>(Resource.Id.E4);
-            var E5 = FindViewById<TextView>(Resource.Id.E5);
-            var E6 = FindViewById<TextView>(Resource.Id.E6);
-
-            // fColumn
-            var F1 = FindViewById<TextView>(Resource.Id.F1);
-            var F2 = FindViewById<TextView>(Resource.Id.F2);
-            var F3 = FindViewById<TextView>(Resource.Id.F3);
-            var F4 = FindViewById<TextView>(Resource.Id.F4);
-            var F5 = FindViewById<TextView>(Resource.Id.F5);
-            var F6 = FindViewById<TextView>(Resource.Id.F6);
-
-            // gColumn
-            var G1 = FindViewById<TextView>(Resource.Id.G1);
-            var G2 = FindViewById<TextView>(Resource.Id.G2);
-            var G3 = FindViewById<TextView>(Resource.Id.G3);
-            var G4 = FindViewById<TextView>(Resource.Id.G4);
-            var G5 = FindViewById<TextView>(Resource.Id.G5);
-            var G6 = FindViewById<TextView>(Resource.Id.G6);
-
-            var whiteOval = Resource.Drawable.
-
-            //gameButton1.Click += (sender, e) => {
-
-            //};
-            gameButton1.Click += async delegate
+           
+        }
+        private void OnClick(object sender, EventArgs args)
+        {
+            if (sender is Button btn)
             {
-
+                var tag = btn.Tag;
+                btn.Click += OnClick;
+                if (tag == gameButton1)
+                {
+                   // method
+                   // DoSomething();
+                }
+                else if (tag == gamebutton2)
+                {
+                   // DoSomethingElse();
+                }
             }
         }
     }
